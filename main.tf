ed1cbbae7ea72b8336a9e6344d5f6a901f5f2f6a
Nafsata <nasfatamohamed323@gmail.com>
2026-01-28 03:55:58 +0100

    Désactivation des références IAM pour corriger le plan Terraform

--- main.tf
+++ main.tf
@@ -21,11 +21,21 @@
   #group = aws_iam_group.dev_group.name
 #}
 
+
 ############################
 # S3 BUCKET
 ############################
+#resource "aws_s3_bucket" "website_bucket" {
+  #bucket = var.bucket_name
+
+  #tags = {
+    #Environment = var.environment
+  #}
+#}`
+
+
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
+  bucket = "mon-site-mohamed"
 
   tags = {
     Environment = var.environment
